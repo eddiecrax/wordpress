3d65e0ada4ea07bf85c1b76dbc2d851a3566bbf2
eddiecrax <eddiecooler001@gmail.com>
2025-02-06 09:40:23 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 provider "google" {
-  credentials = file("D:/ghost/forthbrand-website-project-c95519f0d57b.json")
+  credentials = file("./forthbrand-website-project-c95519f0d57b.json")
   project     = "forthbrand-website-project"
   region      = "us-central1"
 }
